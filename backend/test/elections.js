cb0fd7930b21620fb9d58645822f890b28afa7eb
Fellipe Garcias <fellipegarciasrj@yahoo.com.br>
2021-09-19 19:35:03 -0300

    Primeiro teste unitário para adicionar cidades

--- backend/test/elections.js
+++ backend/test/elections.js
@@ -5,9 +5,21 @@ const Elections = artifacts.require("Elections");
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
-contract("Elections", function (/* accounts */) {
-  it("should assert true", async function () {
-    await Elections.deployed();
-    return assert.isTrue(true);
-  });
+contract("Elections", accounts => {
+
+  describe('Cidades', () => {
+    const name = 'Rio de Janeiro';
+
+    it('1. A cidade não deve ser cadastrada por um usuário não autorizado', () => {
+        return Elections.deployed()
+        .then((instance) => {
+          return instance.addCity(name, { from: accounts[1]});
+        })
+        .then(assert.fail)
+        .catch((e) => {
+          assert(e.message.indexOf('Only the owner can update that information') !== -1);
+        })
+    });
+  })
+
 });
