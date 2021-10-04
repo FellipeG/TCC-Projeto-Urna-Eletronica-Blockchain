212c0daf8cb1b47715af09d18d1a2fd0854a027a
Fellipe Garcias <fellipegarciasrj@yahoo.com.br>
2021-10-03 22:01:35 -0300

    Editando o sh e o readme

--- README.md
+++ README.md
@@ -7,3 +7,13 @@ Este projeto destina-se a implementar um sistema de urna eletrônica que utilize
 * Rode o comando `docker-compose up -d` na pasta do projeto para subir o container
 * Para entrar no container, execute `docker-compose exec app sh`
 * Após isso, rode o sh com o comando `./init.sh` e siga as intruções na tela
+
+# Metamask
+
+Instalar o metamask no navegador através da URL:
+
+https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn
+
+Senha
+
+Senh@TCC
--- init.sh
+++ init.sh
@@ -6,22 +6,27 @@ echo "2 - Executar Testes Unitários"
 echo "Selecione as opções (Separadas por vírgula):"
 read OPTIONS 
 
-cd backend
-
 IFS=',' read -a OPT <<< "$OPTIONS"
 
 for i in "${OPT[@]}"; do
     
     if [ $i -eq "1" ]
     then
+        cd backend
         echo "Executando o ganache..."
-        nohup ganache-cli &
+        ganache-cli -m "oppose hold method produce usual uncle flee subway climb pottery run voyage" -h 0.0.0.0 &
         echo "Executando as migrations..."
-        truffle migrate
+        truffle migrate 
+        cd ../frontend
+        echo "Iniciando o projeto em VueJS"
+        npm run serve &
+        cd ../
     elif [ $i -eq "2" ]
     then
+        cd backend
         echo "Executando testes unitários..."
         truffle test ./test/elections.js
+        cd ../
     else
         echo "Opção inválida!"
         exit 1
