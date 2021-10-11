112aea4e326762b8dbcc8bc27cc4148ea5424636
Fellipe Garcias <fellipegarciasrj@yahoo.com.br>
2021-10-11 16:39:43 -0300

    Página de listagem de cidades com componente de paginação e botão de cadastro

--- frontend/src/views/cities/Cities.vue
+++ frontend/src/views/cities/Cities.vue
@@ -1,30 +1,41 @@
 <template>
-    <div class="container">
-        <table class="table table-striped">
-            <thead class="thead-light">
-                <tr>
-                    <th scope="col">Nome</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-if="cities.length">
-                    <td v-for="city in cities" :key="city">{{ city }}</td>
-                </tr>
-                <tr v-else>
-                    <td>Nenhuma cidade cadastrada</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+        <div class="container pt-150">
+            <base-button type="primary" outline class="float-right mb-2">Cadastrar Cidade</base-button>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-if="cities.length">
+                        <td v-for="city in cities" :key="city">{{ city }}</td>
+                    </tr>
+                    <tr v-else>
+                        <td>Nenhuma cidade cadastrada</td>
+                    </tr>
+                </tbody>
+            </table>
+            <div>
+                <base-pagination :total="cities.length" align="center"></base-pagination>
+            </div>
+        </div>
 </template>
 
 <script>
+
+import BasePagination from "@/components/BasePagination"
+import BaseButton from "@/components/BaseButton"
 
 export default {
     data() {
         return {
             cities: []
         }
+    },
+    components: {
+        BasePagination,
+        BaseButton
     }
 }
 
