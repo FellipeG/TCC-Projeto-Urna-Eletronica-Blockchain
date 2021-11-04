3d74d9cb97e4ab19b9f432b7102e25c568c6901d
Fellipe Garcias <fellipegarciasrj@yahoo.com.br>
2021-11-04 01:38:47 -0300

    Estilização da view de votação + desabilitando o host check

--- frontend/src/views/vote/Vote.vue
+++ frontend/src/views/vote/Vote.vue
@@ -1,10 +1,83 @@
 <template>
         <div class="container pt-150">
+
             <div v-if="!etapaVotacao">
                 Nenhuma votação disponível
             </div>
+
             <div v-else>
-                <span>{{ etapaVotacao.title }}</span>
+                <div class="row">
+                    <div class="col-6">
+
+                        <div class="row">
+                            <div class="col-12">
+                                <h1>{{ etapaVotacao.title }}</h1>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-12">
+                                <base-input v-model="voto" type="number"></base-input>
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-12">
+                                <base-button :outline="true">
+                                    Branco
+                                </base-button>
+                                <base-button type="warning" @click="cleanInput" :outline="true">
+                                    Corrige
+                                </base-button>
+                                <base-button type="success" :outline="true">
+                                    Confirma
+                                </base-button>
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="col-6">
+                        <div v-if="candidate">
+                            <div class="row">
+                                <div class="col-12">
+                                    <h1>{{ candidate.fullName }}</h1>
+                                </div>
+                            </div>
+
+                            <table class="table table-borderless">
+                                <tbody>
+
+                                    <tr>
+                                        <td>Data de Nascimento:</td>
+                                        <td>{{ formatDate(candidate.birthDate) }}</td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>Cidade:</td>
+                                        <td>{{ candidate.city }}</td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>Estado:</td>
+                                        <td>{{ candidate.state }}</td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>Número Eleitoral:</td>
+                                        <td>{{ candidate.electoralNumber }}</td>
+                                    </tr>
+
+                                    <tr>
+                                        <td>Partido Político:</td>
+                                        <td>{{ candidate.politicalParty }}</td>
+                                    </tr>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
                 
             </div>
         </div>
@@ -14,15 +87,20 @@
 
 import BasePagination from "@/components/BasePagination";
 import BaseButton from "@/components/BaseButton";
+import BaseInput from "@/components/BaseInput";
 import Modal from "@/components/Modal";
 import { eventHub } from "@/main";
 import VotationService from "@/services/VotationService";
+import CandidateService from "@/services/CandidateService";
 
 export default {
     data() {
         return {
+            voto: null,
+            votos: [],
             votacaoPosition: 0,
-            votacoes: []
+            votacoes: [],
+            candidate: null
         }
     },
     async created() {
@@ -35,6 +113,13 @@ export default {
         });
     },
     methods: {
+        getCandidateService() {
+            return new CandidateService(
+                this.$store.state.web3,
+                this.$store.state.contract,
+                this.$store.state.accountAddress
+            )
+        },
         getService() {
             return new VotationService(
                 this.$store.state.web3,
@@ -54,9 +139,15 @@ export default {
                 return accounts.indexOf(this.$store.state.accountAddress) !== -1
             });
         },    
-        formatDate(timestamp) {
+        formatDateTimestamp(timestamp) {
             const date = new Date(parseInt(timestamp));
             return date.toLocaleString('pt-BR', {timeZone: 'America/Sao_Paulo'});
+        },
+        cleanInput() {
+            this.voto = null;
+        },
+        formatDate(date) {
+            return date.split('-').reverse().join('/');
         }
     },
     computed: {
@@ -66,9 +157,28 @@ export default {
             }
 
             return this.votacoes[this.votacaoPosition];
+        },
+
+    },
+    watch: {
+        async voto(value) {
+
+            const votationCandidates = (this.etapaVotacao) ? this.etapaVotacao._candidates : [];
+
+            if (votationCandidates.indexOf(value) === -1) {
+                this.candidate = null;
+                return;
+            }
+
+            console.log(value);
+
+            const serviceResponse = await this.getCandidateService().show(value);
+            this.candidate = (serviceResponse) ? serviceResponse.data : null;
         }
     },
     components: {
+        BaseInput,
+        BaseButton
     }
 }
 
