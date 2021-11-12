e02ef0ad8a80d94940496bf2817674d698823a27
Fellipe Garcias <fellipegarciasrj@yahoo.com.br>
2021-11-12 01:05:25 -0300

    Removendo rotas e componentes desnecessários

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,11 +2,6 @@ import Vue from "vue";
 import Router from "vue-router";
 import AppHeader from "./layout/AppHeader.vue";
 import AppFooter from "./layout/AppFooter.vue";
-import Components from "./views/Components.vue";
-import Landing from "./views/Landing.vue";
-import Login from "./views/Login.vue";
-import Register from "./views/Register.vue";
-import Profile from "./views/Profile.vue";
 
 import Home from "./views/home/Home";
 
@@ -249,42 +244,6 @@ export default new Router({
       components: {
         header: AppHeader,
         default: EditPoliticalParty,
-        footer: AppFooter
-      }
-    },
-    {
-      path: "/landing",
-      name: "landing",
-      components: {
-        header: AppHeader,
-        default: Landing,
-        footer: AppFooter
-      }
-    },
-    {
-      path: "/login",
-      name: "login",
-      components: {
-        header: AppHeader,
-        default: Login,
-        footer: AppFooter
-      }
-    },
-    {
-      path: "/register",
-      name: "register",
-      components: {
-        header: AppHeader,
-        default: Register,
-        footer: AppFooter
-      }
-    },
-    {
-      path: "/profile",
-      name: "profile",
-      components: {
-        header: AppHeader,
-        default: Profile,
         footer: AppFooter
       }
     }
